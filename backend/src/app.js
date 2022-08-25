4e1c9851547d4d8db74342c5218ad749cdba969c
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-08-24 23:31:18 -0300

    fix: server header

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,5 +10,12 @@ app.use(formularioRouter);
 app.use(cors());
 app.use(morgan);
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 module.exports = app;
--- frontend/src/app/forms/shared/data.service.ts
+++ frontend/src/app/forms/shared/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { IChoice } from 'src/app/model/IChoice.model';
 
@@ -14,7 +14,12 @@ export class DataService {
 
   public postForms(model: IChoice) {
     console.log(model);
-    this.http.post<Observable<IChoice>>(this.URL, model)
+    /*const params = new HttpHeaders()
+    .set('Access-Control-Allow-Origin', 'http://localhost:4200')
+    .set('Access-Control-Allow-Methods', 'POST')
+    .set('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    .set('Access-Control-Allow-Credentials', 'true');*/
+    this.http.post<Observable<IChoice>>(this.URL, model)//,{headers: params})
       .subscribe(
         (val) => {
           console.log("POST call successful value returned in body",
