da9c5da31e8d1e6bdad249eb3458184dbe382e35
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-08-24 22:54:22 -0300

    fix: returned proxy config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const mysql = require('./database/database');
+//const mysql = require('./database/database');
 
 const port = process.env.PORT || 3333;
 const server = http.createServer(app);
