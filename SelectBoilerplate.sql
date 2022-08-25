40107c66132a08ec66b608b0f5561b641e69f8ec
João Érik da Silva Crisóstomo <79710675+joao-erik2077@users.noreply.github.com>
2022-08-25 10:37:53 -0300

    new: add select

--- SelectBoilerplate.sql
+++ SelectBoilerplate.sql
@@ -17,11 +17,6 @@ FROM formulario
 WHERE p3 = 1;
 
 -- Não fecham a torneira ao escovar os dentes
-SELECT count(*) 'Fecham a torneira ao escovar os dentes'
-FROM formulario
-WHERE p4 = 1;
-
--- Fecham a torneira ao escovar os dentes
 SELECT count(*) 'Não fecham a torneira ao escovar os dentes'
 FROM formulario
 WHERE p4 = 2;
@@ -31,8 +26,20 @@ SELECT count(*) 'Usam fio dental'
 FROM formulario
 WHERE p5 = 1;
 
--- Não sam fio dental
-SELECT count(*) 'Não usam fio dental'
-FROM formulario
-WHERE p5 = 1;
+-- Média dos banhos
+SELECT ROUND(AVG(p3)) 'Média de tempo de banhos'
+FROM formulario;
 
+-- Média dos banhos
+SELECT 
+CASE
+	WHEN ROUND(AVG(p3)) = 1 THEN '5 minutos ou menos'
+    WHEN ROUND(AVG(p3)) = 2 THEN '5-10 minutos'
+    WHEN ROUND(AVG(p3)) = 3 THEN '10-15 minutos'
+    WHEN ROUND(AVG(p3)) = 4 THEN '15 minutos ou mais'
+END 'Tempo médio de banhos'
+FROM formulario;
+
+-- A maioria fecha a torneira ao escovar o dente?
+SELECT IF(ROUND(AVG(p4)) = 1, 'Sim', 'Não') 'A maioria fecha a torneira ao escovar o dente?'
+FROM formulario;
