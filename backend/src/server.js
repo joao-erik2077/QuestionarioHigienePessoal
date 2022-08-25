390fe7dbc528a5b2c6df6efe3c56417a05086ba3
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-08-24 22:34:21 -0300

    fix: changed backend port

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = require('./app');
 const mysql = require('./database/database');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 const server = http.createServer(app);
 
 server.listen(port);
